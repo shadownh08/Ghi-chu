654cec159158534b46e889bdf972f471e8cfaa62
shadownh08 <monkeydluff098@gmail.com>
2025-10-27 15:08:02 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const cacheName="halloween-notes-v1";
-const assets=["./","./index.html","./manifest.json"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(cacheName).then(c=>c.addAll(assets)))});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
